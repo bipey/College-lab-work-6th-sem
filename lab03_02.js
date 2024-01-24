e09d3aaf8d4ab69b4390676ef8ab08f06fdaf64e
bipey <113363191+bipey@users.noreply.github.com>
2024-01-24 19:42:56 +0545

    Update lab03_02.js

--- lab03_02.js
+++ lab03_02.js
@@ -1,7 +1,6 @@
-// Retrieve data from localStorage
+
 var username = localStorage.getItem("username");
 var password = localStorage.getItem("password");
 
-// Display data in the container
 var displayContainer = document.getElementById("displayContainer");
 displayContainer.innerText = "Username: " + username + "\nPassword: " + password;
